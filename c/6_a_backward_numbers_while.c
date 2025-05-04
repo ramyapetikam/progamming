4a3f483b17b6b9f333b27ecf9f84c00ce6c860c7
ramyapetikam <132680654+ramyapetikam@users.noreply.github.com>
2025-05-04 12:41:45 +0530

    Create 6_a_backward_numbers_while.c

---
+++ c/6_a_backward_numbers_while.c
@@ -0,0 +1,14 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+   int n,i;
+   printf("enter n ");
+   scanf("%d",&n);
+   i=n;
+   while(i>=1){
+       printf("%d,",i);
+       i=i-1;
+   }// Write C code here
+    return 0;
+}
