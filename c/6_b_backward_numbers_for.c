9261a55187e0e9ec0e5c8fb80f6b9f05ff80deef
ramyapetikam <132680654+ramyapetikam@users.noreply.github.com>
2025-05-04 13:03:54 +0530

    Create  6_b_backward_numbers_for.c

---
+++ c/6_b_backward_numbers_for.c
@@ -0,0 +1,12 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+   int n;
+   printf("enter n ");
+   scanf("%d",&n);
+  for(int i=n;i>=1;i=i-1){
+      printf("%d , ",i);
+  }// Write C code here
+    return 0;
+}
