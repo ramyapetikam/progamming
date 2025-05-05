a33859d9882b083c92663a74473c8c944c201687
ramyapetikam <132680654+ramyapetikam@users.noreply.github.com>
2025-05-05 20:26:53 +0530

    Create 10_a_nested_for_loop.cpp

---
+++ c++/10_a_nested_for_loop.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+void printStarPattern(int l){
+    for(int i=0;i<l;i++){
+        for(int j=0;j<i+1;j++){
+            cout<<"* ";
+        }
+        cout<<"\n";
+        }
+}
+int main(){
+    int l;
+    cout<<"enter no of lines ";
+    cin>>l;
+    printStarPattern(l);
+
+    return 0;
+}
