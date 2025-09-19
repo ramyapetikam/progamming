c873476b321489ef96cd8d0dd4b4308064812d24
ramyapetikam <132680654+ramyapetikam@users.noreply.github.com>
2025-09-19 21:19:59 +0530

    Update 5_a_numbers_range_while.c

--- c/5_a_numbers_range_while.c
+++ c/5_a_numbers_range_while.c
@@ -1,10 +1,15 @@
+/*
+enter n: 5
+1,2,3,4,5,
+*/
 #include<stdio.h>
 int main(){
     int i=1;
     int n;
-    printf("enter n");
-    scanf("%d,&n");
+    printf("enter n: ");
+    scanf("%d",&n);
     while(i<=n){
-    printf("d,",i);
+    printf("%d,",i);
     i=i+1;}
+return 0;
 }
