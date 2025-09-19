9cad77ca597bed8dd64851068e9808fd10a40eea
ramyapetikam <132680654+ramyapetikam@users.noreply.github.com>
2025-09-19 21:39:03 +0530

    Update 7_a_fibbonacci_numbers.c

--- c/7_a_fibbonacci_numbers.c
+++ c/7_a_fibbonacci_numbers.c
@@ -1,8 +1,11 @@
-// Online C compiler to run C program online
+/*
+enter the number of fibonacci numbers sequence:  6
+fibonacci series 0,1,1,2,3,5,
+*/
 #include <stdio.h>
 int main() {
     int n,first = 0, second = 1, next;
-    printf("enter the number of fibonacci numbers sequence  ");
+    printf("enter the number of fibonacci numbers sequence:  ");
     scanf("%d",&n);
     printf("fibonacci series ");
     for(int i = 0; i<n; i++) {
