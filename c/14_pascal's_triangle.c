f9c8c1e8e84b5c6e93fd9aad06938f9b311c44e9
ramyapetikam <132680654+ramyapetikam@users.noreply.github.com>
2025-09-19 22:25:44 +0530

    Update 14_pascal's_triangle.c

--- c/14_pascal's_triangle.c
+++ c/14_pascal's_triangle.c
@@ -1,1 +1,30 @@
+/*
+enter number of rows: 6
+             1
+           1   1
+         1   2   1
+       1   3   3   1
+     1   4   6   4   1
+   1   5  10  10   5   1
+*/
+#include <stdio.h>
 
+int main() {
+    int rows;
+    printf("enter number of rows: ");
+    scanf("%d",&rows);
+    for (int i = 0 ; i < rows ; i++){
+        int value=1;
+        for (int s = 0 ; s < rows -i-1 ; s++){
+            printf("  ");
+        }
+        for (int j = 0 ; j <= i; j++){
+            printf("%4d",value);
+            value = value *(i - j)/(j + 1);
+        }
+        printf("\n");
+        
+    }
+
+    return 0;
+}
