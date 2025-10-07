a25294e7bbfab5a50d546c8e978ec473a70abd67
ramyapetikam <132680654+ramyapetikam@users.noreply.github.com>
2025-10-07 21:02:03 +0530

    Create 15_bubble_sort.cpp

---
+++ c++/15_bubble_sort.cpp
@@ -0,0 +1,26 @@
+/*
+12456
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    int array[]={1,4,5,2,6};
+    int n=5;
+    int temp;
+    for (int i=0;i<n;i++){
+        for (int j=0;j<n-i-1;j++){
+            if(array[j+1]<array[j]){
+                temp=array[j];
+                array[j]=array[j+1];
+                array[j+1]=temp;
+            }
+            
+        }
+    }
+    for(int i = 0;i<n;i++){
+        cout<<array[i];
+    }
+
+    return 0;
+}
