a2503eb6f7852fed9483212d42f69c6d29d0d6fd
ramyapetikam <132680654+ramyapetikam@users.noreply.github.com>
2025-10-08 22:48:25 +0530

    Update 16_merged_two_arrays.cpp

--- c++/16_merged_two_arrays.cpp
+++ c++/16_merged_two_arrays.cpp
@@ -1,41 +1,42 @@
 /*
-merged two arrays: 12345678910
+    int arr1[]={1 ,3 ,5 ,7 ,9,12,15 };
+    int arr2[]={2 ,4 ,6 ,8 ,10};
+    
+merged two arrays: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 15, 
 */
 #include <iostream>
 using namespace std;
 
 int main() {
-    int arr1[5]={1 ,3 ,5 ,7 ,9 };
-    int arr2[5]={2 ,4 ,6 ,8 ,10};
-    int merged[10];
+    int arr1[]={1 ,3 ,5 ,7 ,9,12,15 };
+    int arr2[]={2 ,4 ,6 ,8 ,10};
     
     int i=0,j=0,k=0;
-    int n1=5,n2=5;
-    
+    int n1=sizeof(arr1)/sizeof(arr1[0]),n2=sizeof(arr2)/sizeof(arr1[0]);
+    int merged[n1+n2];
     while( i < n1 && j < n2 ){
         if(arr1[i]< arr2[j]){
             merged[k]=arr1[i];
             i++;
-    } else {
+        } else {
             merged[k]=arr2[j];
             j++;
-    }
+        }
         k++;
-        }
-        while( i < n1 ){
-            merged[k]=arr1[i];
-            i++;
-            k++;
-        }
-        while( j < n2 ){
-            merged[k]=arr2[j];
-            j++;
-            k++;
-        }
-    // Write C code here
+    }   
+    while( i < n1 ){
+        merged[k]=arr1[i];
+        i++;
+        k++;
+    }
+    while( j < n2 ){
+        merged[k]=arr2[j];
+        j++;
+        k++;
+    }
     cout<<"merged two arrays: ";
     for(int x=0;x<n1+n2;x++){
-        cout<<merged[x];
+        cout<<merged[x]<<", ";
     }
     
 
